26ba82b6538fe8eee70333a53999e658c3fbcd7c
devImroze96 <imroze.linkinpark@gmail.com>
2025-11-10 18:10:07 +0530

    Update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,19 +1,14 @@
 DROP TABLE IF EXISTS recipes;
 
 CREATE TABLE IF NOT EXISTS recipes (
-  id integer PRIMARY KEY AUTO_INCREMENT,
-  -- name of recipe
-  title varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  -- time required to cook/bake the recipe
-  making_time varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  -- number of people the recipe will feed
-  serves varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  -- food items necessary to prepare the recipe
-  ingredients varchar(300) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  -- price of recipe
-  cost integer NOT NULL,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT NOT NULL,
+  making_time TEXT NOT NULL,
+  serves TEXT NOT NULL,
+  ingredients TEXT NOT NULL,
+  cost INTEGER NOT NULL,
+  created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO recipes (
